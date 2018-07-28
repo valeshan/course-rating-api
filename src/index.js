2d9d0c5bad68b6ce0e8306e7b8bcd4b78cced1b7
valeshan <valeshan@gmail.com>
2018-07-28 18:30:17 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// load modules
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/course-rating-api');
+
+const db = mongoose.connection;
+
+//mongo error
+db.on('error', console.error.bind(console, 'connection error'));
+
+//mongo connected to db
+db.on('connected', function(){
+  console.log('Connection to mongoDB successfully opened.');
+})
+
+// set our port
+app.set('port', process.env.PORT || 5000);
+
+// morgan gives us http request logging
+app.use(morgan('dev'));
+
+// TODO add additional routes here
+
+// send a friendly greeting for the root route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the Course Review API'
+  });
+});
+
+// uncomment this route in order to test the global error handler
+// app.get('/error', function (req, res) {
+//   throw new Error('Test error');
+// });
+
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found'
+  })
+})
+
+// global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: {}
+  });
+});
+
+// start listening on our port
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express server is listening on port ${server.address().port}`);
+});
